5ccce0802f2d213284a2e8c5bbb827ecb0b58a80
Q-hj <1485642186@qq.com>
2022-12-09 13:39:10 +0800

    :lipstick:

--- src/pages/screen/prevention/prevention.vue
+++ src/pages/screen/prevention/prevention.vue
@@ -1,13 +1,18 @@
 <!--
  * @Date: 2022-12-09 11:13:55
  * @LastEditors: Mr.qin
- * @LastEditTime: 2022-12-09 11:42:39
+ * @LastEditTime: 2022-12-09 12:00:12
  * @Description: 
 -->
 <script lang="ts" setup></script>
 
 <template>
-	<section class="full-screen">12</section>
+	<section class="full-screen col-between">
+		<header class="flex-center h-70px">
+			<p class="text-30px">震灾预防</p>
+		</header>
+		<main class="flex-1"></main>
+	</section>
 </template>
 
 <style lang="less" scoped>
